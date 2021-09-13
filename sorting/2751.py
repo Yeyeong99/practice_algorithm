aa99334c738c4774d987bd13c43e7bdb7b685f2a
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-09-14 00:50:56 +0900

    백준 / 정렬

--- sorting/2751.py
+++ sorting/2751.py
@@ -1,19 +1,48 @@
+# 시간 복잡도가 중요한 문제 - merge sort 이용해야함.
+
 import sys
 
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    i, j, k = 0, 0, 0 # 각각의 array에서의 요소를 각각 짚어야 하기 때문에.. 예를 들어 left의 두 번째와 right 의 첫 번째를 비교해야하는 경우가 있으니까
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+
+    if i == len(left):
+        while j < len(right):
+            array[k] = right[j]
+            k += 1
+            j += 1
+
+    elif j == len(right):
+        while i < len(left):
+            array[k] = left[i]
+            k += 1
+            i += 1
+    return array
+
 n = int(sys.stdin.readline().rstrip())
+sorting_array = []
 
-sorting = []
+
 for i in range(n):
-    num = int(sys.stdin.readline().rstrip())
-    sorting.append(num)
+    inputted_num = int(sys.stdin.readline().rstrip())
+    sorting_array.append(inputted_num)
 
-for j in range(n):
-    key = sorting[j]
-    target_key = j - 1
-    while target_key >= 0 and sorting[target_key] > key:
-        sorting[target_key], sorting[target_key+1] = sorting[target_key+1], sorting[target_key]
-        target_key -= 1 
+merge_sort(sorting_array)
 
-
-for k in sorting:
-    print(k)
+for j in sorting_array:
+    print(j)
