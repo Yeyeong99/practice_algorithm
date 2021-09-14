b4bbac9911cbd38e70955fe6e3b8165c76de7894
Yeyeong99 <67695343+Yeyeong99@users.noreply.github.com>
2021-09-14 23:28:39 +0900

    백준/ 프린터 큐 / 복습 필요

---
+++ data structure/1966.py
@@ -0,0 +1,26 @@
+#프린터 큐
+import sys
+
+n = int(sys.stdin.readline().rstrip())
+
+for i in range(n):
+    n, m = list(map(int, input().split()))
+    imp = list(map(int, input().split()))
+    idx = list(range(len(imp)))
+    idx[m] = 'target'
+
+    order = 0
+
+    while True:
+        if imp[0] == max(imp):
+            order += 1
+
+            if idx[0] == 'target':
+                print(order)
+                break
+            else:
+                imp.pop(0)
+                idx.pop(0)
+        else:
+            imp.append(imp.pop(0))
+            idx.append(idx.pop(0))
