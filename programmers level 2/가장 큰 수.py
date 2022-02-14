3d2f3dbdc9933514d6cb1c2948e288cac539505c
Yeyeong <67695343+Yeyeong99@users.noreply.github.com>
2022-02-14 17:01:09 +0900

    programmers level 2

--- programmers level 2/가장 큰 수.py
+++ programmers level 2/가장 큰 수.py
@@ -1,11 +1,10 @@
-from itertools import permutations
 def solution(numbers):
-    numbers = list(map(str, list(numbers)))
-    if 0 not in numbers:
-        answer = max(list(map(lambda x: ''.join(x), list(permutations(numbers, len(numbers))))))
-        return answer
-    else:
-        new_numbers = [i for i in numbers if i != '0']
-        answer = max(list(map(lambda x: ''.join(x), list(permutations(new_numbers, len(new_numbers))))))
-        zero = '0' * new_numbers.count('0')
-        return answer + zero
+    numbers = list(map(str, numbers))
+    #numbers의 원소가 1000 이하이기 때문에 세 자리수로 맞춘 후 비교하겠다는 의미
+    # 앞 자리 숫자부터 큰 순서로 차례로 배열해줌
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    
+    # 모든 값이 0인 경우를 처리하기 위해 int로 변환한 후 str로 변환(ex 000 -> 0)
+    return str(int(''.join(numbers)))
+
+# 순열조합을 이용하는 경우 시간 초과
