ff0698f973b71459017cf4f7f88134ed188d277e
Yeyeong <67695343+Yeyeong99@users.noreply.github.com>
2022-04-18 13:36:00 +0900

    백준 17298 스택

--- data structure/17298.py
+++ data structure/17298.py
@@ -10,3 +10,18 @@ for i in range(len(array)):
     elif array[i:][j] < array[i]:
       result[i] = '-1'
 print(' '.join(result))
+
+# 통과
+# 스택을 이용해서 인덱스 저장
+n = int(input())
+array = list(map(int, input().split()))
+
+result = ['-1'] * n
+stack = [0] # index 저장하는 용도
+
+for i in range(1, n):
+  while stack and array[stack[-1]] < array[i]:
+    result[stack.pop()] = array[i]
+  stack.append(i) # 5 4와 같은 경우는 while문을 수행하지 않고 바로 이 줄을 시행함. 그리고 다음 수행에서 pop으로 마지막에 들어간 인덱스를 꺼내줌.
+
+print(*result)
