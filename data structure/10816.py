42a3f482aad71e4895b275b9d95afd2010ba1049
Yeyeong <67695343+Yeyeong99@users.noreply.github.com>
2022-04-18 14:23:07 +0900

    백준 10816 / collections 활용

--- data structure/10816.py
+++ data structure/10816.py
@@ -7,6 +7,7 @@ b = list(map(int, sys.stdin.readline().split()))
 
 answer = []
 
+# 런타임 에러
 for i in b:
     if i in a:
         c = str(a.count(i))
@@ -15,3 +16,46 @@ for i in b:
         answer.append("0")
 
 print(" ".join(answer))
+
+# 시간 초과
+for i in range(m):
+  nums[i] = cards.count(answers[i])
+
+print(*nums)
+
+# 이진탐색 - 시간초과
+n = int(input())
+cards = list(map(int, input().split()))
+m = int(input())
+answers = list(map(int, input().split()))
+
+nums = [0] * m
+
+cards.sort()
+def binary(n, N, start, end):
+  if start > end:
+    return 0
+  mid = (start+end)//2
+  if n == N[mid]:
+    return N[start:end+1].count(n)
+  elif n < N[mid]:
+    return binary(n, N, start, mid-1)
+  else:
+    return binary(n, N, mid+1, end)
+
+for i in range(m):
+  nums[i] = binary(answers[i], cards, 0, len(cards))
+
+print(*nums)
+
+# collections 활용 - 
+from collections import Counter
+
+c = dict(Counter(cards))
+print(c)
+
+for i in range(m):
+  if answers[i] in c.keys():
+    nums[i] = c[answers[i]]
+
+print(*nums)
